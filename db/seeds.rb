f3dd74f9e2693f331a9a239cef58d4a5ffef6ff7
Lauryn <laurynpearson@gmail.com>
2019-08-19 19:45:49 -0500

    create fake student data in seeds with FFaker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+4.times do
 student = Student.create(
-  
+  first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, email: FFaker::Internet.free_email, phone_number: FFaker::PhoneNumber.short_phone_number, bio: FFaker::JobFR.title, linkedin: FFaker::Internet.user_name, twitter: FFaker::Internet.user_name, personal_url: FFaker::Internet.http_url, resume_url: FFaker::Internet.http_url, github_url: FFaker::Internet.http_url, photo: FFaker::Image.url
   )
+end
